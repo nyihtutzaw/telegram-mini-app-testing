b1e8d88ab3bdc50348fce7e014b175864b8c8304
nyihtutzaw <nyihtutzaw.2015@gmail.com>
2024-06-11 12:24:00 +0700

    fix build error

--- src/app/components/CartItem.tsx
+++ src/app/components/CartItem.tsx
@@ -1,4 +1,11 @@
-export function CartItem({ product }) {
+import { FC } from "react";
+import { Product } from "../types";
+
+type Prop={
+  product:Product
+}
+
+export const CartItem:FC<Prop>=({ product })=> {
     return (
       <div className="flex items-center justify-between mb-2">
         <div className="text-sm font-medium text-telegram-black">{product.name}</div>
